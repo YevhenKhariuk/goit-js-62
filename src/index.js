e9da43eb8999a064637d14cd0f80679fa28fddab
YevhenKhariuk <777genya2009@gmail.com>
2022-09-23 22:59:19 +0300

    2ee3e3e

--- src/index.js
+++ src/index.js
@@ -1,1 +1,6 @@
-console.log("hello");
+const productName = "Droid";
+const pricePerItem = 3500;
+
+// Change code below this line
+const message = `You picked ${productName}, price per item is ${pricePerItem} credits`;
+console.info(message);
