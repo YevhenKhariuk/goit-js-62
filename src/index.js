16ecc8cea2d5a5d89968f78264d825febb327f89
YevhenKhariuk <777genya2009@gmail.com>
2022-09-26 23:28:49 +0300

    wef

--- src/index.js
+++ src/index.js
@@ -1,6 +1,28 @@
-const productName = "Droid";
-const pricePerItem = 3500;
+// const pricePerDroid = 800;
+// const orderedQuantity = 6;
+// const deliveryFee = 50;
+// const totalPrice = pricePerDroid * orderedQuantity + deliveryFee;
 
-// Change code below this line
-const message = `You picked ${productName}, price per item is ${pricePerItem} credits`;
-console.info(message);
+// const message = `You ordered droids worth ${totalPrice} credits. Delivery (${deliveryFee} credits) is included in total price.`;
+// console.info(message);
+
+// 1. Оголошення функції multiply
+function add(a, b, c) {
+  return a + b + c;
+}
+let result = add(2, 5, 8);
+console.info(result);
+console.info(add(15, 27, 10));
+console.log(add(10, 20, 30));
+console.log(add(5, 10, 15));
+// let result = add(2, 3, 5);
+// add(15, 27, 10);
+// add(10, 20, 30);
+// add(5, 10, 15);
+
+function makeMessage (name, price) {
+  // Change code below this line
+   const message = ;
+  // Change code above this line
+  return message;
+};
