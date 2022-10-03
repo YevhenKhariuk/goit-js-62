7e96650b0b11f88974abd2942935059afb7fd87c
YevhenKhariuk <777genya2009@gmail.com>
2022-10-03 23:38:52 +0300

    явар

--- src/index.js
+++ src/index.js
@@ -7,22 +7,34 @@
 // console.info(message);
 
 // 1. Оголошення функції multiply
-function add(a, b, c) {
-  return a + b + c;
-}
-let result = add(2, 5, 8);
-console.info(result);
-console.info(add(15, 27, 10));
-console.log(add(10, 20, 30));
-console.log(add(5, 10, 15));
+// function add(a, b, c) {
+//   return a + b + c;
+// }
+// let result = add(2, 5, 8);
+// console.info(result);
+// console.info(add(15, 27, 10));
+// console.log(add(10, 20, 30));
+// console.log(add(5, 10, 15));
 // let result = add(2, 3, 5);
 // add(15, 27, 10);
 // add(10, 20, 30);
 // add(5, 10, 15);
 
-function makeMessage (name, price) {
-  // Change code below this line
-   const message = ;
-  // Change code above this line
-  return message;
-};
+// function makeMessage (name, price) {
+// Change code below this line
+//  const message = ;
+// Change code above this line
+//   return message;
+// };
+
+// // 10
+// function makeMessage (name, price) {
+//   // Change code below this line
+//    const message = `You picked ${name}, price per item is ${price} credits`;
+//   // Change code above this line
+//   return message;
+// };
+// console.log(makeMessage('Radar', 6150));
+// console.log(makeMessage('Scanner', 3500));
+// console.log(makeMessage('Reactor', 8000));
+// console.log(makeMessage('Engine', 4070));
