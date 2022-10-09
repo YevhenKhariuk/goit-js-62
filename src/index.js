65b9c473538e77351c92b7cb6093db1c6478bbba
YevhenKhariuk <777genya2009@gmail.com>
2022-10-09 23:00:47 +0300

    26

--- src/index.js
+++ src/index.js
@@ -146,3 +146,51 @@
 
 //   return canAccessContent;
 // }
+
+// // 23
+// function getDiscount(totalSpent) {
+//   const BASE_DISCOUNT = 0;
+//   const BRONZE_DISCOUNT = 0.02;
+//   const SILVER_DISCOUNT = 0.05;
+//   const GOLD_DISCOUNT = 0.1;
+//   let discount;
+//   // Change code below this line
+//   if (totalSpent >= 50000) {
+//     discount = GOLD_DISCOUNT;
+//   } else if (totalSpent >= 20000 && totalSpent < 50000) {
+//     discount = SILVER_DISCOUNT;
+//   } else if (totalSpent >= 5000 && totalSpent < 20000) {
+//     discount = BRONZE_DISCOUNT;
+//   } else if (totalSpent < 5000) {
+//     discount = BASE_DISCOUNT;
+//   }
+//   // Change code above this line
+//   return discount;
+// }
+
+// // 25
+// function checkStorage(available, ordered) {
+//   let message;
+//   // Change code below this line
+
+//   message =
+//     ordered > available
+//       ? "Not enough goods in stock!"
+//       : "The order is accepted, our manager will contact you";
+
+//   // Change code above this line
+//   return message;
+// }
+
+// // 26
+// function checkPassword(password) {
+//   const ADMIN_PASSWORD = "jqueryismyjam";
+//   let message;
+//   // Change code below this line
+//   message =
+//     password === ADMIN_PASSWORD
+//       ? "Access is allowed"
+//       : "Access denied, wrong password!";
+//   // Change code above this line
+//   return message;
+// }
