d32f84f88ff8f19354e8c84fcf56ef0cbe184777
YevhenKhariuk <777genya2009@gmail.com>
2022-10-10 23:28:17 +0300

    31

--- src/index.js
+++ src/index.js
@@ -241,3 +241,17 @@
 //   // Change code above this line
 //   return message;
 // }
+
+// // 31
+// const courseTopic = "JavaScript essentials";
+// // Change code below this line
+
+// const courseTopicLength = courseTopic.length;
+// const firstElement = courseTopic[0];
+// const lastElement = courseTopic[courseTopic.length - 1];
+
+// // Change code above this line
+
+// console.log(courseTopicLength);
+// console.log(firstElement);
+// console.log(lastElement);
